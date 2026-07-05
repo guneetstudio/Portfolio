960f09ebcf0a56b9ac2840426d18d41a84c00abe
Guneet Singh <guneet@Guneets-MacBook-Air.local>
2026-07-06 02:24:41 +0530

    Update Spine Exports

--- src/data/portfolioItems.generated.ts
+++ src/data/portfolioItems.generated.ts
@@ -75,7 +75,7 @@ export const portfolioItems: PortfolioItem[] = [
     ],
     "spine": {
       "json": "/spine-assets/Chocolate/chocolate.json",
-      "atlas": null,
+      "atlas": "/spine-assets/Chocolate/chocolate.atlas",
       "textures": [
         "/spine-assets/Chocolate/chocolate.png"
       ]
@@ -338,7 +338,7 @@ export const portfolioItems: PortfolioItem[] = [
     ],
     "spine": {
       "json": "/spine-assets/Lollipop/lollipop.json",
-      "atlas": null,
+      "atlas": "/spine-assets/Lollipop/lollipop.atlas",
       "textures": [
         "/spine-assets/Lollipop/lollipop.png"
       ]
